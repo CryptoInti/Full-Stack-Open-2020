f5769a4777880078073aad613e71b2ae72d5915e
CryptoInti <cryptointi@gmail.com>
2020-04-06 03:15:12 -0400

    2a
    Exercises from Unit 2.a

---
+++ part2/2a/exercise2-2.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((sum, part) =>  { return sum + part.exercises }, 0)
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({course}) => {
+  return (
+    <div>
+      {course.parts.map((part, i) => 
+        <Part key={i} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Bleibt!',
+        exercises: 22,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
